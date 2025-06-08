4fc1649bc8ed11966ad66acfda29a4a15be81ae7
Chlukas <lukasnackmayr@proton.me>
2025-06-08 18:01:34 +0200

    feat: added posgresql connection, will be used to store recipes etc.

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,38 +1,75 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"fmt"
+	"log"
 	"math/rand"
+	"os"
 	"time"
 
 	"github.com/ChlukasX/give-me-food/internal/models"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 func main () {
-	amount := flag.Int("amount", 7, "The amount of food recipes")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	amount := flag.Int("amount", 7, "The amount of food recipes")
 	flag.Parse()
 
-	fmt.Printf("The amount is %d\n", *amount)
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+    os.Getenv("DB_HOST"),     // localhost
+    os.Getenv("DB_PORT"),     // 5432
+    os.Getenv("DB_USER"),     // postgres
+    os.Getenv("DB_PASSWORD"), // password
+    os.Getenv("DB_NAME"),     // myapp
+	)
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(connStr)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
+
+	infoLog.Printf("The amount is %d\n", *amount)
 
 	recipes, err := models.GetAll()
 	if err != nil {
-		panic("Oh no")
+		errorLog.Fatal(err)
 	}
 
-	fmt.Println("The Recipes are:")
+	infoLog.Println("The Recipes are:")
 
 	for _, recipe := range recipes {
-		fmt.Println(recipe)
+		infoLog.Println(recipe)
 	}
 
 	rand.Seed(time.Now().UnixNano())
-	fmt.Println("Random Recipes")
+	infoLog.Println("Random Recipes")
 
 	for i := range *amount {
 		randomIndex := rand.Intn(len(recipes))
 		pick := recipes[randomIndex]
-		fmt.Println("nr ", i+1, ": ", pick)
+		infoLog.Println("nr ", i+1, ": ", pick)
 	}
+}
+
+func openDB(connStr string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
